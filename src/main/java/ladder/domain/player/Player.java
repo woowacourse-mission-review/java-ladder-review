17faf67628dc2f4ce28a0d2f2fcc8451ee9e2179
DaejunBae <dpudpu11@gmail.com>
2019-11-24 01:29:39 +0900

    feat: Player 이름으로 논리적 동치성 추가

--- src/main/java/ladder/domain/player/Player.java
+++ src/main/java/ladder/domain/player/Player.java
@@ -2,6 +2,8 @@ package ladder.domain.player;
 
 import ladder.domain.Position;
 
+import java.util.Objects;
+
 public class Player {
     private final PlayerName name;
     private final Position position;
@@ -21,5 +23,26 @@ public class Player {
 
     public Position getPosition() {
         return position;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name=" + name +
+                ", position=" + position +
+                '}';
     }
 }
--- src/test/java/ladder/domain/player/PlayerTest.java
+++ src/test/java/ladder/domain/player/PlayerTest.java
@@ -16,4 +16,14 @@ class PlayerTest {
         assertThat(player.getName()).isEqualTo(name.get());
         assertThat(player.getPosition()).isEqualTo(position);
     }
+
+    @Test
+    void 이름으로_논리적_동치성_테스트() {
+        PlayerName name = PlayerName.from("bedi");
+        Player player1 = Player.of(name, Position.from(0));
+        Player player2 = Player.of(name, Position.from(1));
+
+        assertThat(player1 != player2).isTrue();
+        assertThat(player1).isEqualTo(player2);
+    }
 }
