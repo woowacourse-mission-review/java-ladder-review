e2636853147f9f5a9679d4629714946bb7f75ba3
DaejunBae <dpudpu11@gmail.com>
2019-11-24 01:36:08 +0900

    feat: Players 이름 중복 예외처리

--- src/main/java/ladder/domain/player/Players.java
+++ src/main/java/ladder/domain/player/Players.java
@@ -3,7 +3,9 @@ package ladder.domain.player;
 import ladder.domain.Position;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -16,6 +18,7 @@ public class Players {
 
     private Players(final String inputNames) {
         List<PlayerName> names = parseNames(inputNames);
+        checkDuplicateName(names);
         this.players = createPlayers(names);
     }
 
@@ -29,6 +32,13 @@ public class Players {
                 .collect(Collectors.toList());
     }
 
+    private void checkDuplicateName(final List<PlayerName> names) {
+        Set<PlayerName> set = new HashSet<>(names);
+        if (set.size() != names.size()) {
+            throw new IllegalArgumentException("중복되는 이름이 존재합니다.");
+        }
+    }
+
     private List<Player> createPlayers(final List<PlayerName> names) {
         final List<Player> players = new ArrayList<>();
         int position = MIN_POSITION_BOUNDARY;
@@ -40,5 +50,12 @@ public class Players {
 
     public int size() {
         return players.size();
+    }
+
+    @Override
+    public String toString() {
+        return "Players{" +
+                "players=" + players +
+                '}';
     }
 }
--- src/test/java/ladder/domain/player/PlayersTest.java
+++ src/test/java/ladder/domain/player/PlayersTest.java
@@ -3,6 +3,7 @@ package ladder.domain.player;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PlayersTest {
 
@@ -12,5 +13,12 @@ class PlayersTest {
         Players players = Players.of(inputNames);
 
         assertThat(players.size()).isEqualTo(3);
+    }
+
+    @Test
+    void 이름_중복_예외처리() {
+        String inputNames = "bedi, cu, bedi";
+
+        assertThrows(IllegalArgumentException.class, () -> Players.of(inputNames));
     }
 }
