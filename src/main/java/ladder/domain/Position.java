acaea1f042f5a57f52bb7bb2711a9be36a53502f
DaejunBae <dpudpu11@gmail.com>
2019-11-23 17:26:51 +0900

    feat: Position.class

---
+++ src/main/java/ladder/domain/Position.java
@@ -0,0 +1,59 @@
+package ladder.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Position {
+    static final int POSITION_CACHE_SIZE = 3;
+    static final int POSITION_BOUNDARY = 0;
+
+    private static final Map<Integer, Position> CACHE = new HashMap<>();
+
+    static {
+        for (int i = POSITION_BOUNDARY; i <= POSITION_CACHE_SIZE; i++) {
+            CACHE.put(i, new Position(i));
+        }
+    }
+
+    private final int position;
+
+    private Position(final int position) {
+        validate(position);
+        this.position = position;
+        CACHE.put(position, this);
+    }
+
+    private void validate(final int position) {
+        if (position < POSITION_BOUNDARY) {
+            throw new IllegalArgumentException("포지션은 " + POSITION_BOUNDARY + "이상이어야 합니다.");
+        }
+    }
+
+    public static Position from(final int position) {
+        // TODO: 2019/11/23 소름 돋는다.. 왜 value가 있어도 default가 실행되지??....
+//        return CACHE.getOrDefault(position, new Position(position));
+        final Position value = CACHE.get(position);
+        return value != null ? value : new Position(position);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "position=" + position +
+                '}';
+    }
+}
---
+++ src/test/java/ladder/domain/PositionTest.java
@@ -0,0 +1,32 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static ladder.domain.Position.POSITION_BOUNDARY;
+import static ladder.domain.Position.POSITION_CACHE_SIZE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PositionTest {
+
+    @Test
+    void 캐싱_테스트() {
+        Position position1 = Position.from(1);
+        Position position2 = Position.from(1);
+
+        assertThat(position1 == position2).isTrue();
+    }
+
+    @Test
+    void 캐싱이_안되어있을때_생성하면_캐싱해주는지_확인() {
+        Position position1 = Position.from(POSITION_CACHE_SIZE + 10);
+        Position position2 = Position.from(POSITION_CACHE_SIZE + 10);
+
+        assertThat(position1 == position2).isTrue();
+    }
+
+    @Test
+    void 최소값_미만_예외처리() {
+        assertThrows(IllegalArgumentException.class, () -> Position.from(POSITION_BOUNDARY - 1));
+    }
+}
