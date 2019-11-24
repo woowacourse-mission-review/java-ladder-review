2b0330971c89583b8d0a56ea987aa5233838aed9
skygl <jupiter9501@gmail.com>
2019-11-24 16:14:45 +0900

    feat: HorizonLine, HorizonLines 클래스 추가

---
+++ src/main/java/domain/HorizonLine.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.List;
+
+public class HorizonLine {
+    private final List<LinkedStatus> linkedStatus;
+
+    public HorizonLine(final List<LinkedStatus> linkedStatus) {
+        this.linkedStatus = linkedStatus;
+    }
+}
---
+++ src/main/java/domain/HorizonLines.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.List;
+
+public class HorizonLines {
+    private final List<HorizonLine> horizonLines;
+
+    public HorizonLines(final List<HorizonLine> horizonLines) {
+        this.horizonLines = horizonLines;
+    }
+}
