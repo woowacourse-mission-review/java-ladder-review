f4f0aa234ed1c18e0c1e30650bc0715404575707
green4469 <green4469@gmail.com>
2019-11-24 21:17:37 +0900

    refactor: makes Position class mutable

--- src/main/java/ghostleg/Position.java
+++ src/main/java/ghostleg/Position.java
@@ -2,11 +2,14 @@ package ghostleg;
 
 public class Position {
     private static final int MIN_POSITION = 0;
-    private final int position;
+
+    private int position;
+    private final NumOfPlayers numOfPlayers;
 
     public Position(int position, NumOfPlayers numOfPlayers) {
         validatePosition(position, numOfPlayers);
         this.position = position;
+        this.numOfPlayers = numOfPlayers;
     }
 
     private void validatePosition(int position, NumOfPlayers numOfPlayers) {
@@ -14,5 +17,14 @@ public class Position {
             throw new IllegalArgumentException(String.format("플레이어의 위치는 %d 이상, %d(플레이어의 수) 미만이여야 합니다.",
                     MIN_POSITION, numOfPlayers.getValue()));
         }
+    }
+
+    public void add(int next) {
+        validatePosition(position + next, numOfPlayers);
+        position += next;
+    }
+
+    public int getValue() {
+        return position;
     }
 }
