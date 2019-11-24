f91efef38b3ef37cb85378e02679ac21207ef6e4
green4469 <green4469@gmail.com>
2019-11-24 18:47:06 +0900

    feat: validate the number of points in a line
    The number of points in a line should be equal to the number of players.

--- src/main/java/ghostleg/Line.java
+++ src/main/java/ghostleg/Line.java
@@ -7,10 +7,17 @@ import java.util.ArrayList;
 public class Line {
     private final ArrayList<Point> points;
 
-    public Line(Point... points) {
+    public Line(NumOfPlayers numOfPlayers, Point... points) {
+        validateNumOfPoints(numOfPlayers, points);
         validateFirstAndLast(points);
         validateConsistency(points);
         this.points = Lists.newArrayList(points);
+    }
+
+    private void validateNumOfPoints(NumOfPlayers numOfPlayers, Point[] points) {
+        if (numOfPlayers.getValue() != points.length) {
+            throw new IllegalArgumentException("플레이어의 수와 포인트의 수가 일치해야 합니다.");
+        }
     }
 
     private void validateFirstAndLast(Point[] points) {
--- src/test/java/ghostleg/LineTest.java
+++ src/test/java/ghostleg/LineTest.java
@@ -11,7 +11,8 @@ class LineTest {
 
     @BeforeEach
     void setUp() {
-         testLine = new Line(
+        testLine = new Line(
+                new NumOfPlayers(3),
                 new Point(false, true),
                 new Point(true, false),
                 new Point(false, false));
@@ -35,6 +36,7 @@ class LineTest {
     @Test
     void invalidFirstPoint() {
         assertThrows(IllegalArgumentException.class, () -> new Line(
+                new NumOfPlayers(2),
                 new Point(true, false),
                 new Point(false, false)
         ));
@@ -43,6 +45,7 @@ class LineTest {
     @Test
     void invalidLastPoint() {
         assertThrows(IllegalArgumentException.class, () -> new Line(
+                new NumOfPlayers(2),
                 new Point(false, false),
                 new Point(false, true)
         ));
@@ -51,8 +54,18 @@ class LineTest {
     @Test
     void inconsistentPoints() {
         assertThrows(IllegalArgumentException.class, () -> new Line(
+                new NumOfPlayers(2),
                 new Point(false, true),
                 new Point(false, false)
+        ));
+    }
+
+    @Test
+    void invalidNumOfPoints() {
+        assertThrows(IllegalArgumentException.class, () -> new Line(
+                new NumOfPlayers(3),
+                new Point(false, true),
+                new Point(true, false)
         ));
     }
 }
