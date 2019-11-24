f0ad5f9e71c105e744e6ceed8f64ff816a650883
green4469 <green4469@gmail.com>
2019-11-24 20:13:45 +0900

    refactor: makes exception message string into format string

--- src/main/java/ghostleg/NumOfPlayers.java
+++ src/main/java/ghostleg/NumOfPlayers.java
@@ -12,7 +12,7 @@ public class NumOfPlayers {
 
     private void validateNumOfPlayers(int numOfPlayers) {
         if (numOfPlayers < MIN_NUM_OF_PLAYERS || numOfPlayers > MAX_NUM_OF_PLAYERS) {
-            throw new IllegalArgumentException("플레이어의 수는 최소 2 명 이상, 최대 100 명 이하여야 합니다.");
+            throw new IllegalArgumentException(String.format("플레이어의 수는 최소 %d 명 이상, 최대 %d 명 이하여야 합니다.", MIN_NUM_OF_PLAYERS, MAX_NUM_OF_PLAYERS));
         }
     }
 
